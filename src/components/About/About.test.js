069f14b418e732c77370704f81d5c626f8507f0d
tiffanybacher <tiffanybacher@gmail.com>
2019-06-02 10:46:41 -0600

    Add media queries to Heder and About page

--- src/components/About/About.test.js
+++ src/components/About/About.test.js
@@ -3,13 +3,23 @@ import { shallow } from 'enzyme';
 import About from './About';
 
 describe('About', () => {
+  let mockHistory;
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<About />);
+    mockHistory = { goBack: jest.fn() };
+    wrapper = shallow(
+      <About history={mockHistory} />
+    );
   });
 
   it('should match snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should go back to previous page on click', () => {
+    wrapper.find('.back-btn').simulate('click');
+
+    expect(mockHistory.goBack).toHaveBeenCalled();
   });
 });

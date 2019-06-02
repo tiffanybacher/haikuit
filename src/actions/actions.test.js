002f26dffbb81ed7132859acd4659a6fbfa01f71
tiffanybacher <tiffanybacher@gmail.com>
2019-06-02 15:04:19 -0600

    Test actions

---
+++ src/actions/actions.test.js
@@ -0,0 +1,35 @@
+import * as actions from './index';
+
+describe('actions', () => {
+  let mockHaiku;
+
+  beforeEach(() => {
+    mockHaiku = {
+      title: 'Haiku',
+      line1: 'This is a haiku',
+      line2: 'I hope you like this haiku',
+      line3: 'It\'s just a haiku'
+    }
+  });
+
+  it('should return a type of ADD_HAIKU with a haiku', () => {
+    const result = actions.addHaiku(mockHaiku);
+    const expected = { type: 'ADD_HAIKU', haiku: mockHaiku }
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should return a type of EDIT_HAIKU with a haiku', () => {
+    const result = actions.editHaiku(mockHaiku);
+    const expected = { type: 'EDIT_HAIKU', haiku: mockHaiku }
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should return a type of DELETE_HAIKU with an id', () => {
+    const result = actions.deleteHaiku(mockHaiku.id);
+    const expected = { type: 'DELETE_HAIKU', id: mockHaiku.id }
+
+    expect(result).toEqual(expected);
+  });
+});

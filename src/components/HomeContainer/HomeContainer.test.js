33bc694dae7655e10e6d7f3b07fb8fa891a816af
tiffanybacher <tiffanybacher@gmail.com>
2019-06-02 20:14:08 -0600

    Add redirect test to HomeContaier

--- src/components/HomeContainer/HomeContainer.test.js
+++ src/components/HomeContainer/HomeContainer.test.js
@@ -3,13 +3,21 @@ import { shallow } from 'enzyme';
 import HomeContainer from './HomeContainer';
 
 describe('HomeContainer', () => {
+  let mockHistory;
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<HomeContainer />);
+    mockHistory = [];
+    wrapper = shallow(<HomeContainer history={mockHistory} />);
   });
 
   it('should match snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should direct to /haiku/:id', () => {
+    wrapper.instance().redirect('/haiku/3');
+
+    expect(mockHistory).toEqual(['/haiku/3']);
   });
 });

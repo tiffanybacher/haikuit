56ec6cf7958b6c850769bc18fab72e3ec00fe5cb
tiffanybacher <tiffanybacher@gmail.com>
2019-06-03 19:15:53 -0600

    Test setError action

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -9,26 +9,34 @@ describe('actions', () => {
       line1: 'This is a haiku',
       line2: 'I hope you like this haiku',
       line3: 'It\'s just a haiku'
-    }
+    };
   });
 
   it('should return a type of ADD_HAIKU with a haiku', () => {
     const result = actions.addHaiku(mockHaiku);
-    const expected = { type: 'ADD_HAIKU', haiku: mockHaiku }
+    const expected = { type: 'ADD_HAIKU', haiku: mockHaiku };
 
     expect(result).toEqual(expected);
   });
 
   it('should return a type of EDIT_HAIKU with a haiku', () => {
     const result = actions.editHaiku(mockHaiku);
-    const expected = { type: 'EDIT_HAIKU', haiku: mockHaiku }
+    const expected = { type: 'EDIT_HAIKU', haiku: mockHaiku };
 
     expect(result).toEqual(expected);
   });
 
   it('should return a type of DELETE_HAIKU with an id', () => {
     const result = actions.deleteHaiku(mockHaiku.id);
-    const expected = { type: 'DELETE_HAIKU', id: mockHaiku.id }
+    const expected = { type: 'DELETE_HAIKU', id: mockHaiku.id };
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should return a type of SET_ERROR with an error message', () => {
+    const message = 'Something went wrong';
+    const result = actions.setError(message);
+    const expected = { type: 'SET_ERROR', message };
 
     expect(result).toEqual(expected);
   });

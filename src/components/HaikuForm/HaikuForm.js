55cae53a40faaf86e73871175ecd6c7fd4e983c0
tiffanybacher <tiffanybacher@gmail.com>
2019-06-04 16:38:44 -0600

    Get syllable count for haiku lines

--- src/components/HaikuForm/HaikuForm.js
+++ src/components/HaikuForm/HaikuForm.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import * as actions from '../../actions';
 import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
+import { fetchWord } from '../../apiCalls/fetchWord';
 
 export class HaikuForm extends Component {
   state = {
     title: this.props.title || '',
     line1: this.props.line1 || '',
     line2: this.props.line2 || '',
-    line3: this.props.line3 || ''
+    line3: this.props.line3 || '',
+    line1Syllables: 0,
+    line2Syllables: 0,
+    line3Syllables: 0,
   }
 
   handleChange = (e) => {
@@ -17,6 +21,51 @@ export class HaikuForm extends Component {
     this.setState({
       [name]: value
     });
+  }
+
+  checkAllSyllables = (e) => {
+    e.preventDefault();
+
+    this.checkLineSyllables(1);
+    this.checkLineSyllables(2);
+    this.checkLineSyllables(3);
+  }
+
+  checkLineSyllables = (num) => {
+    const line = this.findLine(num);
+    const lineArray = line.split(' ');
+    let lineCount = 0;
+
+    lineArray.forEach(word => {
+      this.props.fetchWord(word)
+        .then(data => {
+          if (data.syllables) {
+            lineCount += data.syllables.count;
+          } else {
+            lineCount += 1;
+          }
+
+          const key = `line${num}Syllables`;
+
+          this.setState({
+            [key]: lineCount
+          });
+        });
+    });
+  }
+
+  findLine = (num) => {
+    let line;
+
+    if (num === 1) {
+      line = this.state.line1;
+    } else if (num === 2) {
+      line = this.state.line2;
+    } else if (num === 3) {
+      line = this.state.line3;
+    }
+
+    return line;
   }
 
   handleSubmit = (e) => {
@@ -31,7 +80,7 @@ export class HaikuForm extends Component {
 
   render() {
     return (
-      <form className="HaikuForm" onSubmit={this.handleSubmit}>
+      <form className="HaikuForm">
         <label htmlFor="title-input">Title</label>
         <input 
           type="text" 
@@ -61,10 +110,12 @@ export class HaikuForm extends Component {
           value={this.state.line3}
           onChange={this.handleChange} />
          <button 
-           className="HaikuForm-check">
-           Check Syllables
+          onClick={this.checkAllSyllables}
+          className="HaikuForm-check">
+          Check Syllables
         </button>
         <button 
+          onClick={this.handleSubmit}
           className="HaikuForm-submit">
           Submit
        </button>
@@ -74,7 +125,8 @@ export class HaikuForm extends Component {
 }
 
 export const mapDispatchToProps = (dispatch) => ({
-  addHaiku: (haiku) => dispatch(actions.addHaiku(haiku)) 
+  addHaiku: (haiku) => dispatch(actions.addHaiku(haiku)),
+  fetchWord: (word) => dispatch(fetchWord(word))
 });
 
 export default connect(undefined, mapDispatchToProps)(HaikuForm);
@@ -83,5 +135,8 @@ HaikuForm.propTypes = {
   title: PropTypes.string,
   line1: PropTypes.string,
   line2: PropTypes.string,
-  line3: PropTypes.string
+  line3: PropTypes.string,
+  line1Syllables: PropTypes.number,
+  line2Syllables: PropTypes.number,
+  line3Syllables: PropTypes.number,
 }

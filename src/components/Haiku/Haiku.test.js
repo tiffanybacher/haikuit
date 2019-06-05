5f8021ee0ed0d25ff9c5b8a29be1d81a6508472e
tiffanybacher <tiffanybacher@gmail.com>
2019-06-04 23:40:28 -0600

    Update testing for SavedHaiku

--- src/components/Haiku/Haiku.test.js
+++ src/components/Haiku/Haiku.test.js
@@ -3,13 +3,21 @@ import { shallow } from 'enzyme';
 import Haiku from './Haiku';
 
 describe('Haiku', () => {
+  let mockHaiku;
   let wrapper;
 
   beforeEach(() => {
+    mockHaiku = { title: 'title', id: 123 }
     wrapper = shallow(<Haiku />);
   });
 
   it('should match snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should match snapshot if haiku is passed in', () => {
+    wrapper = shallow(<Haiku { ...mockHaiku } />);
+
     expect(wrapper).toMatchSnapshot();
   });
 });

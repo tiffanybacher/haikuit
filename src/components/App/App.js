4237b55bc90be56424bdf32495c06e566baa4e05
tiffanybacher <tiffanybacher@gmail.com>
2019-06-05 01:44:46 -0600

    Create a 404 page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Header from '../Header/Header';
 import Menu from '../Menu/Menu';
@@ -8,6 +8,7 @@ import About from '../About/About';
 import HaikusContainer from '../HaikusContainer/HaikusContainer';
 import EditHaiku from '../EditHaiku/EditHaiku';
 import SavedHaiku from '../SavedHaiku/SavedHaiku';
+import Page404 from '../Page404/Page404';
 
 class App extends Component {
   state = {
@@ -33,11 +34,14 @@ class App extends Component {
           <Header 
             toggleMenu={this.toggleMenu} 
             menuShown={this.state.menuShown} />
+          <Switch>
           <Route exact path="/" component={HomeContainer} />
           <Route exact path="/about" component={About} />
           <Route exact path="/haikus" component={HaikusContainer} />
           <Route exact path="/haiku/:id/edit" component={EditHaiku} />
           <Route exact path="/haiku/:id/saved" component={SavedHaiku} />
+          <Route component={Page404} />
+          </Switch>
         </div>
     );
   }
